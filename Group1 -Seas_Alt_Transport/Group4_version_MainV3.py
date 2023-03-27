e4904e6813d41382e34e11814f5eb02e72edb409
cnorrman <celina.k.norrman@gmail.com>
2023-03-27 10:27:11 +0200

    transport seasonal + altitudinal variations

--- Group1 -Seas_Alt_Transport/Group4_version_MainV3.py
+++ Group1 -Seas_Alt_Transport/Group4_version_MainV3.py
@@ -12,76 +12,240 @@ import matplotlib.colors #To create new colorbar
 # =============================================================================
 
 #USER INPUT - File path
-f_string =r"C:\Users\moheb\Desktop\Q3_Proj (Group Git)\*" #'P:/AE2224I_GroupA4/250hPa/NAmerica/201407/*' #Insert file path to input data, do not forget wildcard
+f_string =r"C:/Users/31683/Desktop/project data/*" #'P:/AE2224I_GroupA4/250hPa/NAmerica/201407/*' #Insert file path to input data, do not forget wildcard
+foldernamelist = ["C:/Users/31683/Desktop/project data/Summer200/*","C:/Users/31683/Desktop/project data/Summer250/*","C:/Users/31683/Desktop/project data/Summer300/*","C:/Users/31683/Desktop/project data/Winter200/*","C:/Users/31683/Desktop/project data/Winter250/*","C:/Users/31683/Desktop/project data/Winter300/*"]
+print(f_string)
+
 
 #USER INPUT - Switches to determine which data types should be loaded
 attila_switch = True
 o3tracer_switch = False
 rad_fluxes_switch = False
+for f_string in foldernamelist:
+    print(f_string)
+    print('h')
 
-#Read in file names based on f_string variable
-filenames_all = sorted(glob.glob(f_string)) #Get all file names in f_string
-print('\n') #Move to next line, improve readability
-print('Files in input folder: ')
-print('\n') #Check that all files present in folder are being printed
-print(filenames_all)
+    #Read in file names based on f_string variable
+    filenames_all = sorted(glob.glob(f_string)) #Get all file names in f_string
+    print('\n') #Move to next line, improve readability
+    print('Files in input folder: ')
+    print('\n') #Check that all files present in folder are being printed
+    print(filenames_all)
 
-#Variables declaration
-time = [] #Attila
-plat = [] #Attila
-plon = [] #Attila
-ppress = [] #Attila in Pa
-airO3_001 = [] #O3lg in mol/mol for emission point 1
-rad_flx_SW = [] #Holds 1 specific call for SW radiative fluxes
-rad_flx_LW = [] #Holds 1 specific call for LW radiative fluxes
-rad_flx_SW_02 = [] #SW flux for call 02
-rad_flx_LW_02 = [] #LW flux for call 02
-global_net_flx = [] #Holds all net fluxes for the 28 EPs (3 months)
+    #Variables declaration
+    time = [] #Attila
+    plat = [] #Attila
+    plon = [] #Attila
+    ppress = [] #Attila in Pa
+    airO3_001 = [] #O3lg in mol/mol for emission point 1
+    rad_flx_SW = [] #Holds 1 specific call for SW radiative fluxes
+    rad_flx_LW = [] #Holds 1 specific call for LW radiative fluxes
+    rad_flx_SW_02 = [] #SW flux for call 02
+    rad_flx_LW_02 = [] #LW flux for call 02
+    global_net_flx = [] #Holds all net fluxes for the 28 EPs (3 months)
 
-#Load relevant variables from NETCDF files
-#Positions of air parcels
-if attila_switch == True:
-    for file in filenames_all:
-        if 'attila.nc' in file:
-            print("HIIIIIIIII")
-            data = Dataset(file,'r')
-            print('\n')
-            print('File loaded: ')
-            print(file)
-            
-            #Latitudes and longitudes
-            lats = data.variables['lat'][:]
-            lons_0to36 = data.variables['lon'][:] #Varies from 0 to 360 deg
-            lons_18to18 = data.variables['lon'][:] #Will be converted later to -180 to 180 deg
-            
-            #changed
-            #Time
-            temp = data.variables['time'][:]
-            time.append(temp)
-           # print(temp)
-            
-            #Air parcel longitudinal position
-            temp = data.variables['PLON'][:]
-            plon.append(temp)
-            
-            #Air parcel latitudinal position
-            temp = data.variables['PLAT'][:]
-            plat.append(temp)
-            
-            #Air parcel pressure altitude
-            temp = data.variables['PPRESS'][:]
-            ppress.append(temp)
+    #Load relevant variables from NETCDF files
+    #Positions of air parcels
+    if attila_switch == True:
+        for file in filenames_all:
+            if 'attila' in file:
+                print("HIIIIIIIII")
+                data = Dataset(file,'r')
+                print('\n')
+                print('File loaded: ')
+                print(file)
+                
+                #Latitudes and longitudes
+                lats = data.variables['lat'][:]
+                lons_0to36 = data.variables['lon'][:] #Varies from 0 to 360 deg
+                lons_18to18 = data.variables['lon'][:] #Will be converted later to -180 to 180 deg
+                
+                #changed
+                #Time
+                temp = data.variables['time'][:]
+                time.append(temp)
+            # print(temp)
+                
+                #Air parcel longitudinal position
+                temp = data.variables['PLON'][:]
+                plon.append(temp)
+                
+                #Air parcel latitudinal position
+                temp = data.variables['PLAT'][:]
+                plat.append(temp)
+                
+                #Air parcel pressure altitude
+                temp = data.variables['PPRESS'][:]
+                ppress.append(temp)
+        
+        
+        #Concatenation of variables, lists become multi-dimensional numpy arrays
+        time = np.concatenate(time, axis=0)
+        plon = np.concatenate(plon, axis=0)
+        plat = np.concatenate(plat, axis=0)
+        ppress = np.concatenate(ppress, axis=0)/100 #Convert to hPa
+        
+        #Convert longitude range from [0, 360] to [-180, 180]
+        plon = (plon + 180) % 360 -180 #Air parcel longitudinal coordinates
+        lons_18to18 = (lons_18to18 + 180) % 360 -180 #EMAC grid longitudes
+
+
+
+
+    step = 10
+    columns = 360/step
+    rows = 180/step
+    EmissionPoint = 0
+    def TrendMap():
+
+        parcel2A = np.arange(EmissionPoint*50,(EmissionPoint+1)*50-1)
+        TrendMapPlot = np.zeros((int(rows),int(columns)))
+        
+        for parcel2Ai in parcel2A:
+            for point in range(len(plon[:,parcel2Ai])):
+                for Nlong in range(int(columns)):
+                    if -180+(Nlong*step) <= plon[point,parcel2Ai] and plon[point,parcel2Ai] <= -180+((Nlong+1)*step):
+                        for Nlat in range(int(rows)):
+                            if 90-(Nlat*step) >= plat[point,parcel2Ai] and plat[point,parcel2Ai] >= 90-((Nlat+1)*step):
+                                TrendMapPlot[Nlat][Nlong]+=1
+        print(np.sum(TrendMapPlot))
+        TrendMapPlot=np.flip(TrendMapPlot,0)
+        return TrendMapPlot
+
+    print(TrendMap())
+
+
+
+    # mohammad's code
+
+
+    flux_list=TrendMap()
+    lat = np.linspace(-90,90,int(rows)+1)  # define x as an array with 4 elements
+    lon = np.linspace(-180,180,int(columns)+1)
+    #Set up axis object for plotting the map
+    fig, ax = plt.subplots() #Subplots are useful for drawing multiple plots together
+        
+    #Adjust dimensions of map plot
+    fig.set_figheight(8)
+    fig.set_figwidth(14)
+        
+    #Define map projection and settings
+    #For more info: https://matplotlib.org/basemap/users/cyl.html
+    mp = Basemap(projection = 'cyl', #equidistant cylindrical projection
+                            llcrnrlon = -180,
+                            llcrnrlat = -90,
+                            urcrnrlon = 180,
+                            urcrnrlat = 90,
+                            resolution = 'i', ax=ax) #h=high, f=full, i=intermediate, c=crude
+        
+    #Shift the fluxes from [0,360] to [-180,180]
+    #net_flx_EP_shft, lons_shft = shiftgrid(180.,global_net_flx[1], lons_0to36,start=False)
+        
+    #Format the lat and lon arrays for map graphing, 
+    #makes lat array a lat x lon array and same for lon array
+    #lon, lat = np.meshgrid(lons_shft, lats)
+    x, y = mp(lon, lat)
+        
+    #Choose the settings for the coastlines, countries, meridians...
+    mp.drawcoastlines(linewidth=0.2)
+    mp.drawcountries(linewidth=0.2)
+        
+    meridians = mp.drawmeridians(np.arange(-180,200,20), 
+                            labels=[False,False,False,True], 
+                            linewidth=0.2, fontsize=10) #Draw lon lines every 20º
+        
+    mp.drawparallels(np.arange(-90,110,20), 
+                            labels=[True,False,False,True], 
+                            linewidth=0.2, fontsize=10) #Draw lat lines every 20º
+        
+    #Set up custom colorbar, colors may be chosen with the help from colorbrewer2.org
+    colors = ["#ffffff", "#fec44f", "#d95f0e", "#e34a33", "#b30000"]
+    cmap= matplotlib.colors.ListedColormap(colors)
+        
+    cmap.set_under("w")
+    cmap.set_over("red")
+        
     
-    
-    #Concatenation of variables, lists become multi-dimensional numpy arrays
-    time = np.concatenate(time, axis=0)
-    plon = np.concatenate(plon, axis=0)
-    plat = np.concatenate(plat, axis=0)
-    ppress = np.concatenate(ppress, axis=0)/100 #Convert to hPa
-    
-    #Convert longitude range from [0, 360] to [-180, 180]
-    plon = (plon + 180) % 360 -180 #Air parcel longitudinal coordinates
-    lons_18to18 = (lons_18to18 + 180) % 360 -180 #EMAC grid longitudes
+    #Plot the flux on the map
+    sc2 = mp.pcolor(x, y, flux_list,
+                        cmap='hot_r',shading='auto')
+        
+    #Define colorbar features
+    cb = fig.colorbar(sc2, extend='both', 
+                        orientation='horizontal',fraction=0.052, 
+                        pad=0.065)
+        
+    #Adjust colorbar tickmark size
+    cb.ax.tick_params(labelsize=14)
+        
+    #Label the colorbar
+    cb.set_label(label="Heat map of all airparcels from the first emission point",size=14,weight='bold')
+        
+        #Save and close the map plot
+    plt.show()
+    plt.close()
+
+'''
+##################
+import numpy as np
+import matplotlib.pyplot as plt
+  
+data = TrendMap(0)
+#Adjust dimensions of map plot
+
+# we can use differenrt cmaps (initial one =>'autumn' )
+m=plt.imshow( data , cmap = 'hot' , interpolation = 'nearest',aspect="auto" )
+
+
+
+
+#3
+fig, ax = plt.subplots() #Subplots are useful for drawing multiple plots together
+
+#Adjust dimensions of map plot
+fig.set_figheight(8)
+fig.set_figwidth(14)
+
+#Define map projection and settings
+#For more info: https://matplotlib.org/basemap/users/cyl.html
+mp = Basemap(projection = 'cyl', #equidistant cylindrical projection
+                        llcrnrlon = -180,
+                        llcrnrlat = -90,
+                        urcrnrlon = 180,
+                        urcrnrlat = 90,
+                        resolution = 'i', ax=ax) #h=high, f=full, i=intermediate, c=crude
+
+#Format the lat and lon arrays for map graphing, 
+#makes lat array a lat x lon array and same for lon array
+lon, lat = np.meshgrid(lons_18to18, lats)
+x, y = mp(lon, lat)
+
+#Choose the settings for the coastlines, countries, meridians...
+mp.drawcoastlines(linewidth=0.2)
+mp.drawcountries(linewidth=0.2)
+
+meridians = mp.drawmeridians(np.arange(-180,200,20), 
+                        labels=[False,False,False,True], 
+                        linewidth=0.2, fontsize=10) #Draw lon lines every 20º
+
+mp.drawparallels(np.arange(-90,110,20), 
+                        labels=[True,False,False,True], 
+                        linewidth=0.2, fontsize=10) #Draw lat lines every 20º
+
+mp.fillcontinents(color='lightgray')
+#3
+
+  
+plt.title( "2-D Heat Map" )
+plt.show()
+'''
+
+
+
+
+
+
+'''
 
 #O3 data along air parcel trajectories
 if o3tracer_switch == True:
@@ -136,7 +300,7 @@ if rad_fluxes_switch == True: #and '250' in f_string:
                 
     #Delete unnecessary variables
     del rad_flx_LW, rad_flx_SW, rad_flx_LW_02, rad_flx_SW_02
-'''''  
+ 
 #Fluxes from the VISO submodel, for 200 and 300 hPa
 if rad_fluxes_switch == True and not '250' in f_string:
     #Start at 3 since first two calls are not emission points
@@ -180,7 +344,8 @@ if rad_fluxes_switch == True and not '250' in f_string:
         print('Net flux for EP'+str(ep-2)+' loaded...')
         
     del rad_flx_LW, rad_flx_SW, rad_flx_LW_02, rad_flx_SW_02
-'''''
+
+
 #Delete unnecessary variables
 if attila_switch or o3tracer_switch or rad_fluxes_switch and not '250' in f_string:
     del temp, data
@@ -193,28 +358,7 @@ if attila_switch or o3tracer_switch or rad_fluxes_switch and not '250' in f_stri
 
 #print(plon[0,parcel10],plat[0,parcel10])
 
-step = 10
-columns = 360/step
-rows = 180/step
 
-def TrendMap(EmissionPoint):
-
-    parcel2A = np.arange(EmissionPoint*50,(EmissionPoint+1)*50-1)
-    TrendMapPlot = np.zeros((int(rows),int(columns)))
-    
-    for parcel2Ai in parcel2A:
-        for point in range(len(plon[:,parcel2Ai])):
-            for Nlong in range(int(columns)):
-                if -180+(Nlong*step) <= plon[point,parcel2Ai] and plon[point,parcel2Ai] <= -180+((Nlong+1)*step):
-                    for Nlat in range(int(rows)):
-                        if 90-(Nlat*step) >= plat[point,parcel2Ai] and plat[point,parcel2Ai] >= 90-((Nlat+1)*step):
-                            TrendMapPlot[Nlat][Nlong]+=1
-    print(np.sum(TrendMapPlot))
-    TrendMapPlot=np.flip(TrendMapPlot,0)
-    return TrendMapPlot
-
-#hello
-print(TrendMap(0))
 
 
 ###########
@@ -595,7 +739,7 @@ if attila_switch == True and rad_fluxes_switch == True:
     plt.close()
 
 
-'''
+
 ##################
 import numpy as np
 import matplotlib.pyplot as plt
@@ -649,72 +793,6 @@ mp.fillcontinents(color='lightgray')
 plt.title( "2-D Heat Map" )
 plt.show()
 '''
-
-flux_list=TrendMap(0)
-lat = np.linspace(-90,90,int(rows)+1)  # define x as an array with 4 elements
-lon = np.linspace(-180,180,int(columns)+1)
-#Set up axis object for plotting the map
-fig, ax = plt.subplots() #Subplots are useful for drawing multiple plots together
-    
-#Adjust dimensions of map plot
-fig.set_figheight(8)
-fig.set_figwidth(14)
-    
-#Define map projection and settings
-#For more info: https://matplotlib.org/basemap/users/cyl.html
-mp = Basemap(projection = 'cyl', #equidistant cylindrical projection
-                         llcrnrlon = -180,
-                         llcrnrlat = -90,
-                         urcrnrlon = 180,
-                         urcrnrlat = 90,
-                         resolution = 'i', ax=ax) #h=high, f=full, i=intermediate, c=crude
-    
-#Shift the fluxes from [0,360] to [-180,180]
-#net_flx_EP_shft, lons_shft = shiftgrid(180.,global_net_flx[1], lons_0to36,start=False)
-    
-#Format the lat and lon arrays for map graphing, 
-#makes lat array a lat x lon array and same for lon array
-#lon, lat = np.meshgrid(lons_shft, lats)
-x, y = mp(lon, lat)
-    
-#Choose the settings for the coastlines, countries, meridians...
-mp.drawcoastlines(linewidth=0.2)
-mp.drawcountries(linewidth=0.2)
-    
-meridians = mp.drawmeridians(np.arange(-180,200,20), 
-                         labels=[False,False,False,True], 
-                         linewidth=0.2, fontsize=10) #Draw lon lines every 20º
-    
-mp.drawparallels(np.arange(-90,110,20), 
-                         labels=[True,False,False,True], 
-                         linewidth=0.2, fontsize=10) #Draw lat lines every 20º
-    
-#Set up custom colorbar, colors may be chosen with the help from colorbrewer2.org
-colors = ["#ffffff", "#fec44f", "#d95f0e", "#e34a33", "#b30000"]
-cmap= matplotlib.colors.ListedColormap(colors)
-    
-cmap.set_under("w")
-cmap.set_over("red")
-    
-   
-#Plot the flux on the map
-sc2 = mp.pcolor(x, y, flux_list,
-                    cmap='hot_r',shading='auto')
-    
-#Define colorbar features
-cb = fig.colorbar(sc2, extend='both', 
-                     orientation='horizontal',fraction=0.052, 
-                      pad=0.065)
-    
-#Adjust colorbar tickmark size
-cb.ax.tick_params(labelsize=14)
-    
-#Label the colorbar
-cb.set_label(label="Heat map of all airparcels from the first emission point",size=14,weight='bold')
-    
-    #Save and close the map plot
-plt.show()
-plt.close()
 
 
 
