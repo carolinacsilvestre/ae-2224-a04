a64f98638fcd9ea52d739ddfd879840f6a7eee7f
AKMielczarek <alex.mielczarek03@gmail.com>
2023-03-30 11:32:44 +0200

    inverted matrix

--- A04_GroupFiles_27 February, 2023 1109/Velocity.py
+++ A04_GroupFiles_27 February, 2023 1109/Velocity.py
@@ -16,10 +16,10 @@ arising from a short-term increase in ozone.'''
 
 
 # JUST KEEP THESE ALL UNCOMMENTED! IT WILL WORK JUST FINE ########################################################3
-# f_string = 'C:/Users/alexm/AE2224/DATA_ANALYSIS/*'
+f_string = 'C:/Users/alexm/AE2224/DATA_ANALYSIS/*'
 # f_string = 'C:/Users/Carolina Silvestre/Desktop/dataproject/*'
 # f_string = 'D:/Python safe/all test data/*'
-f_string = 'E:/all data/Winter 2014 200hpa/*'
+#f_string = 'E:/all data/Winter 2014 200hpa/*'
 
 
 # print('for got sake', f_string)
@@ -408,14 +408,15 @@ for emission_point in range(1, 29):
         MR_arr = np.append(MR_arr, average_mr_one_parcel)
         mean_mr = np.mean(MR_arr)
     n = n + 1  
-    if n == 7:
+    if n == 4:
          m = m + 1
          n = 0
-    print(n,m) 
     fig.set_figheight(16)
     fig.set_figwidth(30)
-    axs[m,n].scatter(RoD_arr, MR_arr * 10E9, s = 5)
-    axs[m,n].set_title(str(emission_point))
+    axs[n,m].scatter(RoD_arr, MR_arr * 10E9, s = 5)
+    axs[n,m].set_title(str(emission_point))
+    # if m == 2 and n == 5:
+    #     print('Avg parcel is', mr_one_parcel, 'yo')
     
     
     
